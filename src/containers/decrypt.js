92e90567eaaa70608def2b12ab41f427ed490889
miquelpuigmena <miquelpuigmena@gmail.com>
2018-10-21 19:55:16 +0200

    Privatekey Voting link to API.

--- src/containers/decrypt.js
+++ src/containers/decrypt.js
@@ -16,35 +16,45 @@ export default class extends Component {
       .then(res => {
         console.log(res);
         console.log(res.data);
-        this.setState({value: res.data});
+        this.setState({ value: res.data });
       }).catch(err => console.log('There was an error:' + err));
-    this.setState({value:''});
+    this.setState({ value: '' });
   }
-  componentDidMount(){
+  componentDidMount() {
     this.getDataList();
   }
 
-  
-  render() {
+
+  render() {/*
+    const data = this.state.value;
+    const permittedIds = array.map(data => data._id);
+    const pos = findIndexOf(x => x.id === _id);
+    pos = 0;
+    const dpoVoted = data[pos].signatures.dpo ? 0:100;
+    const boardVoted = data[pos].signatures.board.length;
+    const boardLength = data[pos].encryptedKeys.boardKeys.length;
+    const encryptedData = data[pos].encryptedData;*/
     return (
+      /*
+      <Route path="/:id/sign" component={Child} />
       <LayoutContentWrapper style={{ height: '100vh' }}>
         <LayoutContent>
           <h1>Decrypt</h1>
-          {/* <div>
-          <span>DPO</span>
-          <Progress completed={}/>
+          <div>
+            <Label bsStyle="success">DPO</Label>
+            <Progress completed={3} />
           </div>
           <div>
-          <span>BOARD</span>
-          <Progress completed={12}/>
+            <Label bsStyle="success">Board</Label>
+            <Progress completed={30 * 100 / 100} />
           </div>
           <div>
-          <span>DATA ENCRYPTED</span>
-          <Progress completed={12}/>
-          </div> */}
-
+            <Label bsStyle="primary">Data Encrypted</Label>
+            <Progress completed={"encryptedData"} />
+          </div>
         </LayoutContent>
-      </LayoutContentWrapper>
+      </LayoutContentWrapper>*/
+      <div></div>
     );
   }
 }
